ad623133016fabd61c840514f6037dfee799e5f6
vitropez <yagoaltafaj@hotmail.com>
2022-01-04 13:52:34 +0100

    eventos

---
+++ src/components/Eventos.js
@@ -0,0 +1,90 @@
+import React,{Component} from "react";
+export  class EventosES6 extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            contador:0
+        };
+
+        this.sumar=this.sumar.bind(this);
+        this.restar=this.restar.bind(this);
+    }
+
+    sumar(e){
+
+this.setState({
+    contador:this.state.contador+1,
+});
+
+} 
+    restar(e){
+        this.setState({
+            contador:this.state.contador-1,
+
+    });
+}
+
+render(){
+    return(
+        <div>
+            <h2>Eventos en Componentes de Clase ES6</h2>
+           
+            <nav>
+            <button onClick={this.sumar}>
+            +
+            </button>
+            <button onClick={this.restar}>
+            -
+            </button >
+
+            </nav>
+            <h3>{this.state.contador}</h3>
+        </div>
+    );
+}
+
+}
+
+export  class EventosES7 extends Component{
+
+    state={
+        contador:0
+    };
+
+  
+
+
+sumar=(e)=>{
+
+this.setState({
+contador:this.state.contador+1,
+});
+
+} 
+restar=(e)=>{
+    this.setState({
+        contador:this.state.contador-1,
+
+});
+}
+
+render(){
+return(
+    <div>
+        <h2>Eventos en Componentes de Clase ES7</h2>
+       
+        <nav>
+        <button onClick={this.sumar}>
+        +
+        </button>
+        <button onClick={this.restar}>
+        -
+        </button >
+
+        </nav>
+        <h3>{this.state.contador}</h3>
+    </div>
+);
+}
+
+}
